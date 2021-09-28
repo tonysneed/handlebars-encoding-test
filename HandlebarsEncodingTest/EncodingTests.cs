c205261e3c5ba8fc1acf916c60e2317405563160
Anthony Sneed <tony.sneed@icloud.com>
2021-09-28 15:49:33 -0500

    Add project files.

---
+++ HandlebarsEncodingTest/EncodingTests.cs
@@ -0,0 +1,157 @@
+﻿using HandlebarsDotNet;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace HandlebarsEncodingTest
+{
+    public partial class EncodingTests
+    {
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Handlebars_Should_Encode_Chinese(bool useAnnotations)
+        {
+            // Arrange
+            RegisterPartialTemplates();
+            var classTemplate = CompileClassTemplate();
+            var data = GetTemplateData(useAnnotations);
+            Handlebars.Configuration.NoEscape = true; // Do not escape Chinese characters
+
+            // Act
+            string actual = classTemplate(data);
+
+            // Assert
+            var expected = useAnnotations
+                ? ExpectedEntitiesNoEscapeAnnotations.ProductClass
+                : ExpectedEntitiesNoEscapeNoAnnotations.ProductClass;
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void Handlebars_Should_Not_Encode_Chinese(bool useAnnotations)
+        {
+            // Arrange
+            RegisterPartialTemplates();
+            var classTemplate = CompileClassTemplate();
+            var data = GetTemplateData(useAnnotations);
+            Handlebars.Configuration.NoEscape = false; // Escape Chinese characters
+
+            // Act
+            string actual = classTemplate(data);
+
+            // Assert
+            var expected = useAnnotations
+                ? ExpectedEntitiesEscapeAnnotations.ProductClass
+                : ExpectedEntitiesEscapeNoAnnotations.ProductClass;
+            Assert.Equal(expected, actual);
+        }
+
+        private Dictionary<string, object> GetTemplateData(bool useDataAnnotations)
+        {
+            var templateData = new Dictionary<string, object>();
+            templateData.Add("use-data-annotations", useDataAnnotations);
+            templateData.Add("namespace", "FakeNamespace");
+            templateData.Add("comment", "    /// 产品");
+
+            AddClassAnnotations(templateData, useDataAnnotations);
+            AddClassProperties(templateData, useDataAnnotations);
+
+            return templateData;
+        }
+
+        private void AddClassAnnotations(Dictionary<string, object> templateData, bool useDataAnnotations)
+        {
+            if (!useDataAnnotations) return;
+            var classAnnotationsData = new List<Dictionary<string, object>>
+            {
+                new()
+                {
+                    { "class-annotation", "[Table(\"Product\")]" }
+                },
+                new()
+                {
+                    { "class-annotation", "[Index(nameof(CategoryId), Name = \"IX_Product_CategoryId\")]" }
+                },
+            };
+            templateData.Add("class-annotations", classAnnotationsData);
+        }
+
+        private void AddClassProperties(Dictionary<string, object> templateData, bool useDataAnnotations)
+        {
+            var classPropertiesData = new List<Dictionary<string, object>>
+            {
+                new()
+                {
+                    { "property-type", "int" },
+                    { "property-name", "ProductId" },
+                    { "property-comment", "        /// 编号"},
+                },
+                new()
+                {
+                    { "property-type", "string" },
+                    { "property-name", "ProductName" },
+                    { "property-comment", "        /// 名称"},
+                },
+            };
+            if (useDataAnnotations)
+            {
+                classPropertiesData[0].Add("property-annotations", new List<Dictionary<string, object>>
+                {
+                    new()
+                    {
+                        {"property-annotation", "[Key]"},
+                    }
+                });
+                classPropertiesData[1].Add("property-annotations", new List<Dictionary<string, object>>
+                {
+                    new()
+                    {
+                        {"property-annotation", "[Required]"},
+                    },
+                    new()
+                    {
+                        {"property-annotation", "[StringLength(40)]"},
+                    },
+                    new()
+                    {
+                        {"property-annotation", "[Column(TypeName = \"money\")]"},
+                    },
+                });
+            }
+            templateData.Add("properties", classPropertiesData);
+        }
+
+        private void RegisterPartialTemplates()
+        {
+            var partialTemplates = GetPartialTemplates();
+            foreach (var partialTemplate in partialTemplates)
+            {
+                Handlebars.RegisterTemplate(partialTemplate.Key, partialTemplate.Value);
+            }
+        }
+
+        private Dictionary<string, string> GetPartialTemplates()
+        {
+            var ctorTemplateFile = File.ReadAllText("CodeTemplates/CSharpEntityType/Partials/Constructor.hbs");
+            var importsTemplateFile = File.ReadAllText("CodeTemplates/CSharpEntityType/Partials/Imports.hbs");
+            var propsTemplateFile = File.ReadAllText("CodeTemplates/CSharpEntityType/Partials/Properties.hbs");
+
+            var partialTemplateFiles = new Dictionary<string, string>
+            {
+                { "constructor", ctorTemplateFile },
+                { "imports", importsTemplateFile },
+                { "properties", propsTemplateFile },
+            };
+            return partialTemplateFiles;
+        }
+
+        private HandlebarsTemplate<object, object> CompileClassTemplate()
+        {
+            var classTemplateFile = File.ReadAllText("CodeTemplates/CSharpEntityType/Class.hbs");
+            return Handlebars.Compile(classTemplateFile);
+        }
+    }
+}
---
+++ HandlebarsEncodingTest/ExpectedEntities.cs
@@ -0,0 +1,135 @@
+﻿namespace HandlebarsEncodingTest
+{
+    public partial class EncodingTests
+    {
+        private static class ExpectedEntitiesNoEscapeNoAnnotations
+        {
+            public const string ProductClass =
+                @"using System;
+using System.Collections.Generic;
+
+namespace FakeNamespace
+{
+    /// <summary>
+    /// 产品
+    /// </summary>
+    public partial class 
+    {
+
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string ProductName { get; set; }
+    }
+}
+";
+        }
+
+        private static class ExpectedEntitiesNoEscapeAnnotations
+        {
+            public const string ProductClass =
+                @"using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeNamespace
+{
+    /// <summary>
+    /// 产品
+    /// </summary>
+    [Table(""Product"")]
+    [Index(nameof(CategoryId), Name = ""IX_Product_CategoryId"")]
+    public partial class 
+    {
+
+        /// <summary>
+        /// 编号
+        /// </summary>
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        [Required]
+        [StringLength(40)]
+        [Column(TypeName = ""money"")]
+        public string ProductName { get; set; }
+    }
+}
+";
+        }
+
+        private static class ExpectedEntitiesEscapeNoAnnotations
+        {
+            public const string ProductClass =
+                @"using System;
+using System.Collections.Generic;
+
+namespace FakeNamespace
+{
+    /// <summary>
+    /// &#20135;&#21697;
+    /// </summary>
+    public partial class 
+    {
+
+        /// <summary>
+        /// &#32534;&#21495;
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// &#21517;&#31216;
+        /// </summary>
+        public string ProductName { get; set; }
+    }
+}
+";
+        }
+
+        private static class ExpectedEntitiesEscapeAnnotations
+        {
+            public const string ProductClass =
+                @"using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeNamespace
+{
+    /// <summary>
+    /// &#20135;&#21697;
+    /// </summary>
+    [Table(""Product"")]
+    [Index(nameof(CategoryId), Name = ""IX_Product_CategoryId"")]
+    public partial class 
+    {
+
+        /// <summary>
+        /// &#32534;&#21495;
+        /// </summary>
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// &#21517;&#31216;
+        /// </summary>
+        [Required]
+        [StringLength(40)]
+        [Column(TypeName = ""money"")]
+        public string ProductName { get; set; }
+    }
+}
+";
+        }
+    }
+}
